e8af5e72805217ce245ed6ce3b9d0ed564522423
Kemal Aditya Zulfikar <kadityafikar@gmail.com>
2021-05-26 00:40:35 +0700

    add card

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 import { Container, Grid } from '@material-ui/core';
 import React, { Component } from 'react';
-import { ChevronUp } from 'react-feather';
+import { ArrowDown, ChevronUp, MoreVertical } from 'react-feather';
 import Navbar from '../../shared/Navbar';
 import SideNav from '../../shared/SideNav';
 import help from '../../assets/icons/help.png'
+import sales from '../../assets/icons/sales.svg'
 
 class dashboard extends Component {
     render() {
@@ -28,7 +29,24 @@ class dashboard extends Component {
                                 <ChevronUp />
                             </div>
                         </div>
-                        <Grid xs={3} className='cart'>dfsadfasdf</Grid>
+                        <Grid xs={3} className='cart'>
+                            <div className="title">
+                                Sales Turnover
+                                <MoreVertical />
+                            </div>
+                            <div className="nominal">
+                                <div className="price">
+                                    <p>Rp 3,600,000</p>
+                                    <div className='desc'>
+                                            <ArrowDown/> 
+                                        <b> 
+                                            13.8% <span>last period in product sold</span> 
+                                        </b>
+                                    </div>
+                                </div>
+                                <img src={sales} alt="sales" />
+                            </div>
+                        </Grid>
                     </div>
                 </div>
                 </Container>
